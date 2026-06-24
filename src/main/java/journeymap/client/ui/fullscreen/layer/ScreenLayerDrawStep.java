c56aaa4363bbab652e280a277e47a45f61485bb1
ABKQPO <93412322+ABKQPO@users.noreply.github.com>
2026-06-24 11:25:13 +0800

    document smooth map zoom
    add language entries for the smooth zoom option.
    
    update the changelog and add a marker for fixed screen-space fullscreen overlays.

---
+++ src/main/java/journeymap/client/ui/fullscreen/layer/ScreenLayerDrawStep.java
@@ -0,0 +1,15 @@
+/*
+ * JourneyMap Mod <journeymap.info> for Minecraft
+ * Copyright (c) 2011-2017  Techbrew Interactive, LLC <techbrew.net>.  All Rights Reserved.
+ */
+
+package journeymap.client.ui.fullscreen.layer;
+
+import journeymap.client.render.draw.DrawStep;
+
+/**
+ * Marks fullscreen overlay draw steps that should stay in screen space instead of map space.
+ */
+public interface ScreenLayerDrawStep extends DrawStep
+{
+}
