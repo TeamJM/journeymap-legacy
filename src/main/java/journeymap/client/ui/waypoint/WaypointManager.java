da8bbb35885006ea37bb3e3117969bd73ef05014
Algent <algent@gmail.com>
2026-07-07 16:41:17 +0200

    Disable key-repeat events in onGuiClosed instead of closeAndReturn, so repeats don't leak when the screen closes by another path

--- src/main/java/journeymap/client/ui/waypoint/WaypointManager.java
+++ src/main/java/journeymap/client/ui/waypoint/WaypointManager.java
@@ -688,8 +688,6 @@ public class WaypointManager extends JmUI
         Fullscreen.state().requireRefresh();
         bottomButtons.setEnabled(true);
 
-        Keyboard.enableRepeatEvents(false);
-
         if (returnDisplay == null)
         {
             UIManager.getInstance().closeAll();
@@ -698,6 +696,12 @@ public class WaypointManager extends JmUI
         {
             UIManager.getInstance().open(returnDisplay);
         }
+    }
+
+    @Override
+    public void onGuiClosed()
+    {
+        Keyboard.enableRepeatEvents(false);
     }
 
     Map<Config.Category, List<SlotMetadata>> getToolbars()
